2b1b7cb7ea82f5f9fc23748eedf46594caba9ca3
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2020-02-16 14:23:00 +1300

    MAJOR: rework

---
+++ src/Traits/Cacher.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Sunnysideup\AssetsOverview\Traits;
+
+use Psr\SimpleCache\CacheInterface;
+
+trait Cacher
+{
+
+    private static $cacheCache = null;
+
+    /**
+     * @return CacheInterface
+     */
+    protected static function getCache()
+    {
+        if (self::$cacheCache == null) {
+            self::$cacheCache = Injector::inst()->get(CacheInterface::class . '.assetsoverviewCache');
+        }
+        return self::$cacheCache;
+    }
+
+    /**
+     * @param string  $cacheKey
+     * @param mixed   $value
+     */
+    protected static function setCacheValue(string $cacheKey, $value)
+    {
+        $cache = self::getCache();
+
+        $cache->set($cacheKey, serialize($value));
+    }
+
+    /**
+     * @param string  $cacheKey
+     *
+     * @return mixed
+     */
+    protected static function getCacheValue(string $cacheKey)
+    {
+        $cache = self::getCache();
+
+        return unserialize($cache->get($cacheKey));
+    }
+
+    /**
+     *
+     * @param  string $cacheKey
+     * @return bool
+     */
+    protected function hasCacheKey(string $cacheKey) : bool
+    {
+        $cache = self::getCache();
+
+        return $cache->has($cacheKey);
+    }
+
+}
